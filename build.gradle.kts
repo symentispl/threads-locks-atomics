fc626e49844ba5cface420b93dd67aebbefa6169
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2021-10-16 00:08:14 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import com.github.jrubygradle.api.core.RepositoryHandlerExtension
+
+plugins {
+    id("org.asciidoctor.jvm.revealjs") version "3.3.2"
+    id("org.asciidoctor.jvm.gems") version "3.3.2"
+    id("org.kordamp.gradle.livereload") version "0.4.0"
+}
+
+repositories {
+    gradlePluginPortal()
+    // https://github.com/jruby-gradle/jruby-gradle-plugin/issues/407
+    this as ExtensionAware
+    the<RepositoryHandlerExtension>().gems()
+}
+
+dependencies {
+    dependencies {
+        asciidoctorGems("rubygems:asciidoctor-revealjs:4.1.0")
+    }
+}
+
+tasks.asciidoctorRevealJs {
+    sourceDir("src/main/slides")
+    sources {
+        include("index.adoc")
+    }
+    setOutputDir("build/slides")
+    resources {
+        from("src/main/resources") {
+            include("**")
+        }
+    }
+}
+
+tasks.liveReload {
+    setDocRoot("build/slides")
+}
